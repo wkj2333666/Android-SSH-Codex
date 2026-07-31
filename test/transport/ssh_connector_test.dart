4349d2fea2aa46e67a846a10135630341a1f69e9
wkj2333666 <2695071759@qq.com>
2026-08-01 00:49:09 +0800

    test: reproduce empty SSH passphrase failure

--- test/transport/ssh_connector_test.dart
+++ test/transport/ssh_connector_test.dart
@@ -2,6 +2,15 @@ import 'package:android_ssh_codex/src/transport/ssh_connector.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  test('normalizes empty private key passphrases to null', () {
+    expect(normalizePrivateKeyPassphrase(''), isNull);
+    expect(normalizePrivateKeyPassphrase('   '), isNull);
+  });
+
+  test('preserves non-empty private key passphrases exactly', () {
+    expect(normalizePrivateKeyPassphrase('  secret  '), '  secret  ');
+  });
+
   test('formats host keys as standard SHA256 base64 fingerprints', () {
     expect(
       formatHostKeyFingerprint(List.filled(32, 0)),
